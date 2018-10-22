c2c56d14321a8ec816fe8043b6613f29a4415af7
David Strauss <david_strauss@microcall.com>
2018-10-22 12:46:41 -0400

    done son!

--- js/main2.js
+++ js/main2.js
@@ -2,7 +2,7 @@
 
 (function(){
     var output = document.getElementById("book-list-data"),
-    sortButton = documnet.getElementById("sort-by-Author"),
+    sortButton = document.getElementById("sort-by-Author"),
     clearButton = document.getElementById("clear-button"),
     addButton= document.getElementById("add-button"),
     titleText= document.getElementById("title"),
@@ -11,18 +11,66 @@
     publishYear=  document.getElementById("publish-year"),
     books= [];
 
-var Book = function Book(title,authorFirst,authorLast,publishYear){
-    this.title= title;
+var Book = function Book(titleText,authorFirst,authorLast,publishYear){
+    this.title= titleText;
     this.AuthorFirstName = authorFirst;
     this.AuthorLastName = authorLast;
     this.Year  = publishYear;
+    this.construct = "<p>" + this.title + " by " + this.AuthorFirstName + " " + this.AuthorLastName + " published in " + this.Year + "</p>";
 };
 
+    var renderList = function renderList(){
+        //TODO loop over list of book and write them.
+        output.innerHTML = " ";
+        books.forEach(function(book,idx,arr){
+           // output.innerHTML += book.construct;
+            var newDiv = document.createElement("div"),
+                newHeading = document.createElement("h2"),
+                newPara = document.createElement("p");
+
+            newHeading.innerText = book.title;
+            newPara.innerHTML = "<em>By</em> " + book.AuthorFirstName + " " + book.AuthorLastName + " Published in " + book.Year;
+            newDiv.appendChild(newHeading);
+            newDiv.appendChild(newPara);
+            output.appendChild(newDiv);
+        });
+        
+        // for(var i=0; i<books.length;i++){
+        //     output.innerHTML += books[i].construct;
+        // }
+    };
+
     addButton.addEventListener("click",function(){
-        output.innerHTML = "";
-        var newBook = Book(titleText.value, authorFirst.value, authorLast.value, publishYear.value);
+        //output.innerHTML = "";
+        var newBook = new Book(titleText.value, authorFirst.value, authorLast.value, publishYear.value);
         books.push(newBook);
-        output.innerHTML += books;
+        renderList();
     
-    })
+    });
+
+    clearButton.addEventListener("click",function(){
+        titleText.value = "";
+        authorFirst.value ="";
+        authorLast.value="";
+        publishYear.value="";
+    });
+
+    sortButton.addEventListener("click",function(){
+        books = books.sort(function(a,b){
+            var nameA = a.AuthorLastName.toUpperCase();
+            var nameB = b.AuthorLastName.toUpperCase();
+            if(nameA < nameB){
+                return -1;
+            }
+            if(nameA > nameB){
+                return 1;
+            }
+            return 0;
+        });
+
+        renderList();
+
+    });
+
+
 })();
